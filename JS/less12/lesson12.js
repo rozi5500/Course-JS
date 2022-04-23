f7614f0efdc1d757cb896e9cc793c204d9a73a0f
Nick <kolyabogach12@gmail.com>
2022-04-23 20:30:37 +0300

    l12

--- JS/less12/lesson12.js
+++ JS/less12/lesson12.js
@@ -1,60 +1,60 @@
 // Recursiona
 // GitHub
 
-// array = [-145, -4, -716, -19, -15, -53];
+array = [-145, -4, -716, -19, -15, -53];
 
-// count = Infinity;
+count = Infinity;
 
-// array.forEach(element => {
-//   if(element < count){
-//     count = element;
-//   }
-// });
+array.forEach(element => {
+  if(element < count){
+    count = element;
+  }
+});
 
-// console.log(count);
+console.log(count);
 
-// const deepObj = {
-//   one: {
-//     two: {
-//       three: {}
-//     }
-//   },
-//   innerOne: {
-//     innerTwo: {
-//       innerThree: {
-//         four: 4,
-//         six: { },
-//         five: [
-//           { user: { name: 'Victor' } }
-//         ]
-//       }
-//     }
-//   }
-// };
+const deepObj = {
+  one: {
+    two: {
+      three: {}
+    }
+  },
+  innerOne: {
+    innerTwo: {
+      innerThree: {
+        four: 4,
+        six: { },
+        five: [
+          { user: { name: 'Victor' } }
+        ]
+      }
+    }
+  }
+};
 
-// let deepCount = -Infinity;
+let deepCount = -Infinity;
 
-// const deepCounter = (obj, count = 0) => {
-//   if (typeof obj !== 'object' && obj !== null) {
-//     if (deepCount < count) {
-//       deepCount = count;
-//     }
+const deepCounter = (obj, count = 0) => {
+  if (typeof obj !== 'object' && obj !== null) {
+    if (deepCount < count) {
+      deepCount = count;
+    }
 
-//     return;
-//   }
+    return;
+  }
 
-//   const objValues = Object.values(obj);
+  const objValues = Object.values(obj);
 
-//   for (const value of objValues) {
-//     deepCounter(value, ++count);
-//   }
+  for (const value of objValues) {
+    deepCounter(value, ++count);
+  }
   
 
-//   return deepCount;
+  return deepCount;
   
-// }
+}
 
-// const result = deepCounter(deepObj);
+const result = deepCounter(deepObj);
 
-// console.log(result);
+console.log(result);
 
