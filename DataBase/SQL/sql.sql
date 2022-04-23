ef9af240f0e6f82ac0d7b22af2111503f9ccf90c
Nick <kolyabogach12@gmail.com>
2022-04-23 20:34:01 +0300

    SQL

--- DataBase/SQL/sql.sql
+++ DataBase/SQL/sql.sql
@@ -1,36 +1,36 @@
--- SELECT * FROM students;
+SELECT * FROM students;
 
--- select * from students where age = 15;
+select * from students where age = 15;
 
--- select * from students where name = 'dima';
+select * from students where name = 'dima';
 
--- select name from students;
+select name from students;
 
--- select * from students where name like 'o%';
+select * from students where name like 'o%';
 
--- select * from students where name like '%a';
+select * from students where name like '%a';
 
--- select * from students where name like 'r%n';
+select * from students where name like 'r%n';
 
--- select * from students where name like '___a%';
+select * from students where name like '___a%';
 
--- select * from students order by age desc;
+select * from students order by age desc;
 
--- select * from students where gender like 'f%' order by name;
+select * from students where gender like 'f%' order by name;
 
--- select * from students where id >= 4 order by age;
+select * from students where id >= 4 order by age;
 
--- select * from students where id >3 and gender like 'm%' order by age;
+select * from students where id >3 and gender like 'm%' order by age;
 
--- select * from students where age <= 20 or gender like 'f%' order by age;
+select * from students where age <= 20 or gender like 'f%' order by age;
 
 select * from students order by age asc, name desc;
 
--- select * from students where age between 10 and 35;
--- select * from students where age >= 10 and age <= 35;
+select * from students where age between 10 and 35;
+select * from students where age >= 10 and age <= 35;
 
--- select * from students where age = 18 or age = 31;
--- select * from students where age in (18, 31);
+select * from students where age = 18 or age = 31;
+select * from students where age in (18, 31);
 
 select min(age) from students;
 select max(age) from students;
