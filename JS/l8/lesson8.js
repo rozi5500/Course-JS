2d6f3a6d55e62535ac5052cceb4a2595e25fdf9e
Nick <kolyabogach12@gmail.com>
2022-04-23 20:22:35 +0300

    Promise

--- JS/l8/lesson8.js
+++ JS/l8/lesson8.js
@@ -1,110 +1,110 @@
 
 
-// function oderStatement(money) {
-//     return new Promise((resolve, reject) => {
-//         setTimeout(() => {
-//             if (money > 450) {
-//                 resolve({country:'The United States', change: money - 450});
-//             } else {
-//                 reject('We don\'t have enough money')
-//             }
+function oderStatement(money) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (money > 450) {
+                resolve({country:'The United States', change: money - 450});
+            } else {
+                reject('We don\'t have enough money')
+            }
 
-//         }, 1100);
-//     })
-// }
+        }, 1100);
+    })
+}
 
-// function preparing() {
-//     return new Promise((resolve) => {
-//         setTimeout(() => {
-//             resolve('I\'m ready')
-//         }, 900);
-//     })
-// }
+function preparing() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve('I\'m ready')
+        }, 900);
+    })
+}
 
-// function goToAirport(data) {
-//     return new Promise((resolve, reject) => {
-//         console.log('I bought tickets on', data);
-//         setTimeout(() => {
-//             if (data === 23.02){
-//                 return reject(`Tickets on ${data} haven't left`)
-//             }
+function goToAirport(data) {
+    return new Promise((resolve, reject) => {
+        console.log('I bought tickets on', data);
+        setTimeout(() => {
+            if (data === 23.02){
+                return reject(`Tickets on ${data} haven't left`)
+            }
 
-//             resolve('Chuh chuh')
-//         }, 940);
-//     })
-// }
+            resolve('Chuh chuh')
+        }, 940);
+    })
+}
 
-// oderStatement(460).then((value) => {
-//     console.log('Oh yes, we go to', value);
-//     console.log('Збирай шмотки');
-    
-//     return preparing(); 
-// })
-// .then((state) => {
-//     console.log(state);
+oderStatement(460).then((value) => {
+    console.log('Oh yes, we go to', value);
+    console.log('Збирай шмотки');
 
-//     return goToAirport(22.02)
-// })
-// .then((value) => {
-//     console.log(value);
-// })
+    return preparing();
+})
+.then((state) => {
+    console.log(state);
 
-// .catch((reason) => {
-//     console.log(`We can't go to vacation because ${reason}`);
-// })
+    return goToAirport(22.02)
+})
+.then((value) => {
+    console.log(value);
+})
 
-// const send1 = new Promise((resolve) => {
-//     setTimeout(() => {
-//         resolve('Send1')
-//     }, 2000);
-// })
-// const send2 = new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//         reject('Send2')
-//     }, 1000);
+.catch((reason) => {
+    console.log(`We can't go to vacation because ${reason}`);
+})
 
-// })
-// const send3 = new Promise((resolve) => {
-//     setTimeout(() => {
-//         resolve('Send3');
-//     }, 1000);
-// })
+const send1 = new Promise((resolve) => {
+    setTimeout(() => {
+        resolve('Send1')
+    }, 2000);
+})
+const send2 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        reject('Send2')
+    }, 1000);
 
-// console.time('1')
+})
+const send3 = new Promise((resolve) => {
+    setTimeout(() => {
+        resolve('Send3');
+    }, 1000);
+})
 
-// Promise.all([send1, send2.catch, send3]).then((value) => {
-//     console.log(value);
-//     console.timeEnd('1')
-// })
+console.time('1')
 
-
-// Promise.allSettled([send1, send2, send3])
-//     .then((value) => {
-//         console.log(value);
-//     })
+Promise.all([send1, send2.catch, send3]).then((value) => {
+    console.log(value);
+    console.timeEnd('1')
+})
 
 
-// async function vacation() {
-//     try {
-//         const value = await oderStatement(500);
-//     console.log('Oh yes, we go to', value.country); 
-//     console.log(`${value.change} left`);
-//     console.log('Збирай шмотки');
+Promise.allSettled([send1, send2, send3])
+    .then((value) => {
+        console.log(value);
+    })
 
-//     const ready = await preparing();
-//     console.log(ready);
 
-//     const roadToAirPort = await goToAirport(23.02);
-//     console.log(roadToAirPort);
-        
-//     } catch (error) {
-//         console.log(error);
-//     }
+async function vacation() {
+    try {
+        const value = await oderStatement(500);
+    console.log('Oh yes, we go to', value.country);
+    console.log(`${value.change} left`);
+    console.log('Збирай шмотки');
 
-    
-// }
+    const ready = await preparing();
+    console.log(ready);
 
-// vacation();
+    const roadToAirPort = await goToAirport(23.02);
+    console.log(roadToAirPort);
+
+    } catch (error) {
+        console.log(error);
+    }
+
+
+}
+
+vacation();
 
 function wakeUp(time) {
     return new Promise((resolve, reject) => {
@@ -173,68 +173,68 @@ function swimmingPools(){
     })
 }
 
-// wakeUp(10)
-//     .then((value) => {
-//         console.log(value);
+wakeUp(10)
+    .then((value) => {
+        console.log(value);
 
-//         return eat();
-//     })
-//     .then((value) => {
-//         console.log(value);
-//         return shower();
-//     })
-//     .then((value) => {
-//         console.log(value);
-//         return zaryadka();
-//     })
-//     .then((value) => {
-//         console.log(value);
-//         return bus(10);
-//     })
-//     .then((value) => {
-//         console.log(value);
-//         return post();
-//     })
-//     .then((value) => {
-//         console.log(value);
-//         return swimmingPools();
-//     })
-//     .then((value) => {
-//         console.log(value);
-//     })
-//     .catch((error) => {
-//         console.log(error);
-//     })
-
-
-// async function actions() {
-//     try {
-//         const wakeup = await wakeUp(10);
-//         console.log(wakeup);
-    
-//         const eat1 = await eat();
-//         console.log(eat1, 'fruit');
-    
-//         const takingShower = await shower();
-//         console.log(takingShower);
-    
-//         const physicalExercises = await zaryadka();
-//         console.log(physicalExercises);
-    
-//         const waitingForBus = await bus(50);
-//         console.log(waitingForBus, `Осталось ${change} грошей`);
-    
-//         const goToPost = await post();
-//         console.log(goToPost);
-    
-//         const goToSwim = await swimmingPools();
-//         console.log(goToSwim);
-//     } catch (error) {
-//         console.log(error);
-//     }
-
-   
-// }
+        return eat();
+    })
+    .then((value) => {
+        console.log(value);
+        return shower();
+    })
+    .then((value) => {
+        console.log(value);
+        return zaryadka();
+    })
+    .then((value) => {
+        console.log(value);
+        return bus(10);
+    })
+    .then((value) => {
+        console.log(value);
+        return post();
+    })
+    .then((value) => {
+        console.log(value);
+        return swimmingPools();
+    })
+    .then((value) => {
+        console.log(value);
+    })
+    .catch((error) => {
+        console.log(error);
+    })
 
 
-// actions();
+async function actions() {
+    try {
+        const wakeup = await wakeUp(10);
+        console.log(wakeup);
+
+        const eat1 = await eat();
+        console.log(eat1, 'fruit');
+
+        const takingShower = await shower();
+        console.log(takingShower);
+
+        const physicalExercises = await zaryadka();
+        console.log(physicalExercises);
+
+        const waitingForBus = await bus(50);
+        console.log(waitingForBus, `Осталось ${change} грошей`);
+
+        const goToPost = await post();
+        console.log(goToPost);
+
+        const goToSwim = await swimmingPools();
+        console.log(goToSwim);
+    } catch (error) {
+        console.log(error);
+    }
+
+
+}
+
+
+actions();
