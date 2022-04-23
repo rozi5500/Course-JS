7e6a3996219888748e64b63e5163aa67a3897fa1
Nick <kolyabogach12@gmail.com>
2022-04-23 15:22:04 +0300

    Promises, Array methods

--- l10/lesson10.js
+++ l10/lesson10.js
@@ -1,250 +1,250 @@
-// const cheburek = {
+const cheburek = {
 
-//     name: 'Mouse',
-//     age: 25,
-//     job: true
-// }
+    name: 'Mouse',
+    age: 25,
+    job: true
+}
 
-// cheburek.wife = 'Jennifer';
+cheburek.wife = 'Jennifer';
 
-// const array = ['Petya', 'Kolya', 'Vova', 'Victor', 'Anna', cheburek, { name: 'Nataly' }]
+const array = ['Petya', 'Kolya', 'Vova', 'Victor', 'Anna', cheburek, { name: 'Nataly' }]
 
-// Диструктуризация
-// const [n1, n2, ...others] = array;
+// Диструктуризація
+const [n1, n2, ...others] = array;
 
-// console.log(others);
+console.log(others);
 
-// for (let i = 0; i < array.length; i+=2) {
-//     console.log(array[i]);
-    
-// }
+for (let i = 0; i < array.length; i+=2) {
+    console.log(array[i]);
 
+}
 
-// for (const iterator of array) {
-//     console.log(iterator);
-// }
 
+for (const iterator of array) {
+    console.log(iterator);
+}
 
-// Чтобы вытянуть значения ключей обьекта динамично 
-// в масивном стиле
-// for (const key in cheburek) {
-//     console.log(cheburek[key]);
-// }
 
-// function test(name = 'Yurii') {
-//     arrFromArg = Array.from(arguments);
-//     arrFromArgCopy = [...arguments];
+// Для того щоб витягнути значення ключів об'єкта динамічно
+// в масивному стилі
+for (const key in cheburek) {
+    console.log(cheburek[key]);
+}
 
-//     console.log(arrFromArg);
-//     console.log(`Yo ${name}`);
-// }
+function test(name = 'Yurii') {
+    arrFromArg = Array.from(arguments);
+    arrFromArgCopy = [...arguments];
 
-// test('Dima', 'Vasya', 'Petya');
+    console.log(arrFromArg);
+    console.log(`Yo ${name}`);
+}
 
-// const suplier = {
-//     name: 'Jorj',
-//     age: 29,
-//     car: true
-// }
+test('Dima', 'Vasya', 'Petya');
 
-// const city = {
-//     name: 'Vancouver',
-//     age: 170
-// }
+const suplier = {
+    name: 'Jorj',
+    age: 29,
+    car: true
+}
 
+const city = {
+    name: 'Vancouver',
+    age: 170
+}
 
-// const { name: suplierName } = suplier;
-// const { name: cityName } = city;
 
-// console.log(suplierName, 'from', cityName);
+const { name: suplierName } = suplier;
+const { name: cityName } = city;
 
+console.log(suplierName, 'from', cityName);
 
 
-// const array = ['Petya', 'Kolya', 'Vova', 'Victor', 'Anna',  { name: 'Nataly' }]
 
-// const include = array.includes('Kolya');
+const array = ['Petya', 'Kolya', 'Vova', 'Victor', 'Anna',  { name: 'Nataly' }]
 
-// const deletedName = array.pop();
+const include = array.includes('Kolya');
 
+const deletedName = array.pop();
 
-// const map = array.map((value, index) => {
-//     return { 
-//         id: index + 1, 
-//         name:value
-//     }
-// })
 
+const map = array.map((value, index) => {
+    return {
+        id: index + 1,
+        name:value
+    }
+})
 
 
-// const cheburek = {
-//     name: 'Mouse',
-//     age: 25,
-//     job: true
-// }
 
+const cheburek = {
+    name: 'Mouse',
+    age: 25,
+    job: true
+}
 
-// // Це робиться для того щоб зробити копію
-// // об'єкта щоб не було мутації
-// const ObjCopy = JSON.parse(JSON.stringify(cheburek));
 
-// ObjCopy.name = 'palitra';
+// Це робиться для того щоб зробити копію
+// об'єкта щоб не було мутації
+const ObjCopy = JSON.parse(JSON.stringify(cheburek));
 
-// test()
+ObjCopy.name = 'palitra';
 
-// function test() {
-//     console.log('test');
-// }
+test()
 
+function test() {
+    console.log('test');
+}
 
 
-// const func = () => {
-//     console.log('func');
-// }
 
-//  func()
+const func = () => {
+    console.log('func');
+}
 
-// function test() {
-//     let x = 0;
+ func()
 
-//     return () =>{
-//         x++;
+function test() {
+    let x = 0;
 
-//         console.log(x);
-//     }
-// }
+    return () =>{
+        x++;
 
-// const num = test();
+        console.log(x);
+    }
+}
 
+const num = test();
 
 
-// const p1 = new Promise((resolve, reject) => {
-//     resolve('Hello p1')
-// })
 
+const p1 = new Promise((resolve, reject) => {
+    resolve('Hello p1')
+})
 
 
-// const p2 = new Promise((resolve, reject) => {
-//     resolve('Hello p2')
-// })
 
+const p2 = new Promise((resolve, reject) => {
+    resolve('Hello p2')
+})
 
-// p1
-//     .then(value => {
-//         console.log(value);
 
-//         return p2;
-//     })
-//     .then(value => {
-//         console.log(value);
-//     })
-//     .catch(err => {
-//         console.error(err);
-//     })
+p1
+    .then(value => {
+        console.log(value);
 
-// Promise.allSettled([p1, p2]). then(value => {
-//     console.log(value);
-// })
+        return p2;
+    })
+    .then(value => {
+        console.log(value);
+    })
+    .catch(err => {
+        console.error(err);
+    })
 
-// async function test() {
-//     try {
-//         const waitP1 = await p1;
-//         const waitP2 = await p2;
+Promise.allSettled([p1, p2]). then(value => {
+    console.log(value);
+})
 
-//         console.log(waitP1);
-//         console.log(waitP2);
-//     } catch (error) {
-//         console.log(error);
-//     }
-    
-// }
+async function test() {
+    try {
+        const waitP1 = await p1;
+        const waitP2 = await p2;
 
-// test();
+        console.log(waitP1);
+        console.log(waitP2);
+    } catch (error) {
+        console.log(error);
+    }
 
-// const array = ['Petya', 'Kolya', 'Vova', 'Victor', 'Anna',  { name: 'Nataly' }]
+}
 
-// array.sort((a, b) => {
-//     if (a > b) {
-//         return 1;
-//     } 
-//         return -1
-// });
+test();
 
-// array.forEach(element => {
-//     if(element.length <= 4){
-//         return
-//     }
-//     console.log(element);
-// });
+const array = ['Petya', 'Kolya', 'Vova', 'Victor', 'Anna',  { name: 'Nataly' }]
 
-// // Шукає перший елемент який сходиться інструкцією
-// const find = array.find(element => element.endsWith('a'));
-// console.log(find);
+array.sort((a, b) => {
+    if (a > b) {
+        return 1;
+    }
+        return -1
+});
 
-// const findI = array.findIndex(element => element.startsWith('A'));
-// console.log(findI);
+array.forEach(element => {
+    if(element.length <= 4){
+        return
+    }
+    console.log(element);
+});
 
-// const filter = array.filter(element => element.length >= 4);
-// console.log(filter);
+// Шукає перший елемент який сходиться інструкцією
+const find = array.find(element => element.endsWith('a'));
+console.log(find);
 
-// const some = array.some(element => element.startsWith('A'));
-// console.log(some);
+const findI = array.findIndex(element => element.startsWith('A'));
+console.log(findI);
 
-// const every = array.every(element => element.length >= 4);
-// console.log(every);
+const filter = array.filter(element => element.length >= 4);
+console.log(filter);
 
-// const users = [
-//     { name: 'Dima', age: 10 },
-//     { name: 'Nasta', age: 22 },
-//     { name: 'Volodya', age: 19 },
-//     { name: 'Yarik', age: 39 },
-//     { name: 'Jenya', age: 14 },
-//     { name: 'Taras', age: 44 }
-// ];
+const some = array.some(element => element.startsWith('A'));
+console.log(some);
 
-// const reduce = users.reduce((acc, value) => {
+const every = array.every(element => element.length >= 4);
+console.log(every);
 
-//     return acc + value.age
-// }, 0)
+const users = [
+    { name: 'Dima', age: 10 },
+    { name: 'Nasta', age: 22 },
+    { name: 'Volodya', age: 19 },
+    { name: 'Yarik', age: 39 },
+    { name: 'Jenya', age: 14 },
+    { name: 'Taras', age: 44 }
+];
 
-// console.log(reduce);
+const reduce = users.reduce((acc, value) => {
 
+    return acc + value.age
+}, 0)
 
-// const age = 103;
+console.log(reduce);
 
-// const checkAge = age >= 103 ? 'Yes' : 'No';
 
-// console.log(checkAge);
+const age = 103;
 
-// console.time('1')
-// for (let i = 0; i < 15; i++) {
-//     console.log(i);
-    
-// }
+const checkAge = age >= 103 ? 'Yes' : 'No';
 
-// console.timeEnd('1')
+console.log(checkAge);
 
-// const names = 'Ira;Vasya;Petya;Do;Re';
-// const split = names.split(';');
+console.time('1')
+for (let i = 0; i < 15; i++) {
+    console.log(i);
 
-// const map = split.map((value, index) => {
-//     return {
-//         id: index + 1,
-//         name: value
-//     }
-// })
+}
 
-// console.log(map);
+console.timeEnd('1')
 
-// const text = 'Hello I had a turtle and I\'m expressed by a turtle\'s behavior. I did like turtle';
-// const replaced = text.replaceAll('turtle', 'Pig')
+const names = 'Ira;Vasya;Petya;Do;Re';
+const split = names.split(';');
 
-// console.log(replaced);
+const map = split.map((value, index) => {
+    return {
+        id: index + 1,
+        name: value
+    }
+})
+
+console.log(map);
+
+const text = 'Hello I had a turtle and I\'m expressed by a turtle\'s behavior. I did like turtle';
+const replaced = text.replaceAll('turtle', 'Pig')
+
+console.log(replaced);
 
 
 const obj = {
     name: 'Jin',
     age: 305,
-    wife: false, 
+    wife: false,
     job: {
         pos: 'Senior',
         since: 2020
