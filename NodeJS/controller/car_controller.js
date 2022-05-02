678857442ee4d0d96fdaa4ce7f2817314d4070b9
Nick <kolyabogach12@gmail.com>
2022-05-02 15:59:07 +0300

    update

--- NodeJS/controller/car_controller.js
+++ NodeJS/controller/car_controller.js
@@ -1,10 +1,10 @@
 
 let cars = [
-  { id: 0, name: "BMW", year: 2020},
-  { id: 1, name: "Mazda", year: 2002},
-  { id: 2, name: "Porshe", year: 2022},
-  { id: 3, name: "Toyota", year: 1996},
-  { id: 4, name: "Range Rover", year: 2015}
+  { id: 0, name: "BMW", year: 2020 },
+  { id: 1, name: "Mazda", year: 2002 },
+  { id: 2, name: "Porshe", year: 2022 },
+  { id: 3, name: "Toyota", year: 1996 },
+  { id: 4, name: "Range Rover", year: 2015 }
 ]
 
 module.exports = {
